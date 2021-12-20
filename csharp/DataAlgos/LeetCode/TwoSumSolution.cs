b061e519fe184426b8e5e8fd7309af032459f855
James Meyer <jamesjmeyer210@gmail.com>
2021-12-20 17:41:14 -0600

    added solution documentation

--- csharp/DataAlgos/LeetCode/TwoSumSolution.cs
+++ csharp/DataAlgos/LeetCode/TwoSumSolution.cs
@@ -3,8 +3,20 @@ using System.Collections.Generic;
 
 namespace LeetCode
 {
+    /// <summary>
+    /// Solution to the 2Sum Problem.
+    /// Runtime: 128 ms, faster than 90.09% of C# online submissions for Two Sum.
+    /// Memory Usage: 42.7 MB, less than 26.69% of C# online submissions for Two Sum.
+    /// </summary>
     public static class TwoSumSolution
     {
+        /// <summary>
+        /// Time Complexity:
+        /// <code>O(n) = (n * 2) + n </code>
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
         public static int[] TwoSum(int[] nums, int target)
         {
             var set = new HashSet<int>(nums);
